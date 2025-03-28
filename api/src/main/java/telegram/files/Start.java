60aa144b25a953164ad44182af0514046f7727ce
jarvis2f <137974272+jarvis2f@users.noreply.github.com>
2025-03-28 21:48:59 +0800

    🔖 0.1.17

--- api/src/main/java/telegram/files/Start.java
+++ api/src/main/java/telegram/files/Start.java
@@ -13,7 +13,7 @@ public class Start {
 
     private static final Log log = LogFactory.get();
 
-    public static final String VERSION = "0.1.16";
+    public static final String VERSION = "0.1.17";
 
     private static final CountDownLatch shutdownLatch = new CountDownLatch(1);
 
